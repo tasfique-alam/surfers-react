2b798726884333ae93bfdb36826c51c1a3467093
tasfique-alam <tasfiquealam@gmail.com>
2021-08-06 21:21:30 +0600

    icons added

--- src/layouts/Public/Navbar/index.js
+++ src/layouts/Public/Navbar/index.js
@@ -1,52 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Row, Col, Form, ListGroup } from "react-bootstrap";
- const Navbar = () => {
+import { FcBriefcase } from 'react-icons/fc';
+import { BiAddToQueue, BiArchive } from 'react-icons/bi';
+
+const Navbar = () => {
+
+const [akash, setAkash] = useState(false)
+
     return (
+
+
+
         <div className="banner">
-          <nav className="navbar navbar-expand-lg navbar-light  sticky-top">
-              <div className="container">
-                  <a className="navbar-brand" href="#">
-                      <img className="navbar-brand-logo" src="images/logo.png"></img>
-                  </a>
-                  <button class="navbar-toggler bg-light" type="button" data-toggle="collapse" data-target="#navbarResponsive">
-                     <span class="navbar-toggler-icon"></span>
-                  </button>
+            <nav className="navbar navbar-expand-lg navbar-light  sticky-top">
+                <div className="container">
+                    <a className="navbar-brand" href="#">
+                        <img className="navbar-brand-logo" src="images/logo.png"></img>
+                    </a>
+                    <button class="navbar-toggler bg-light" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
                     <div className="collapse navbar-collapse" id="navbarResponsive">
                         <ul id="top-bar" className="navbar-nav">
-                           <li className="nav-item">
-                               <a class="nav-link active" href="#boards">
-                                 Boards
-                              </a>
-                          </li>
+                            <li className={akash ? 'navitem active' : 'navitem'} onClick={()=>setAkash(!akash)}>
+                                <a class="nav-link" href="#boards">
+                                    boards
+                                    <FcBriefcase className="" />
+                                </a>
+                            </li>
                             <li className="nav-item">
-                               <a class="nav-link active" href="about-us">
-                                 About us
-                              </a>
+                                <a class="nav-link active" href="about-us">
+                                    About us
+                                </a>
                             </li>
                             <li className="nav-item">
                                 <a class="nav-link active" href="#ou-team">
-                                 our-team
+                                    our-team
                                 </a>
                             </li>
                             <li className="nav-item">
                                 <a class="nav-link active" href="#technology">
-                                 Technology
+                                    Technology
                                 </a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link active" href="#team">
-                                  Team
+                                    Team
                                 </a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link active" href="#dealers">
-                                 dealers
+                                    dealers
                                 </a>
                             </li>
                         </ul>
                     </div>
-              </div>
-           </nav>
+                </div>
+            </nav>
             <div className="banner-main text-center">
                 <h1 className="banner-main-title">
                     Ride every wave as if it's your last
